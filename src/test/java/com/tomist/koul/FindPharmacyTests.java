3ada22e686b35ec1e90bba8439c2fe8c7fc2a18e
Koul <dongik0122@gmail.com>
2024-09-10 13:52:54 +0900

    feat : setup
    개발을 위한 셋업 구성
    1. 스프링부트 의존성 설정
    2. 도커파일 작성

---
+++ src/test/java/com/tomist/koul/FindPharmacyTests.java
@@ -0,0 +1,13 @@
+package com.tomist.koul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindPharmacyTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
